156563b3bad9ac846f30f4ffe812a27903234fbe
Federico Kesselman <kesselmanfederico@gmail.com>
2024-09-09 14:01:25 -0300

    OO1 Ej1 y Ej2

---
+++ SEGUNDO AÑO/2do CUATRIMESTRE/Orientación a Objetos 1 (OO1)/OO1 Maven/OO1 Maven/src/main/java/ar/edu/info/unlp/ejercicio2/Producto.java
@@ -0,0 +1,42 @@
+package ar.edu.info.unlp.ejercicio2;
+
+public class Producto {
+	private double peso;
+	private double precioPorKilo;
+	private String descripcion;
+	
+	public double getPeso() {
+		return peso;
+	}
+
+	public double getPrecioPorKilo() {
+		return precioPorKilo;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public double getPrecio() {
+		return this.precioPorKilo * this.peso;
+	}
+
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+
+	public void setPrecioPorKilo(double precioPorKilo) {
+		this.precioPorKilo = precioPorKilo;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+		
+}
---
+++ SEGUNDO AÑO/2do CUATRIMESTRE/Orientación a Objetos 1 (OO1)/ejercicio1/src/main/java/ar/edu/unlp/oo1/ejercicio1/impl/WallPostImpl.java
@@ -0,0 +1,63 @@
+package ar.edu.unlp.oo1.ejercicio1.impl;
+
+import ar.edu.unlp.oo1.ejercicio1.WallPost;
+
+/**
+ * Completar esta clase de acuerdo a lo especificado en el cuadernillo
+ *
+ */
+public class WallPostImpl implements WallPost {
+	private String text;
+	private int likes;
+	private boolean featured;
+
+    public WallPostImpl() {
+    	this.text = "Undefined post";
+    	this.likes = 0;
+    	this.featured = false;
+    }
+    
+    public String getText() {
+		return this.text;
+	}
+
+	@Override
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public int getLikes() {
+		return this.likes;
+	}
+
+	@Override
+	public void like() {
+		this.likes++;
+	}
+
+	@Override
+	public void dislike() {
+		if (this.getLikes() > 0) this.likes--;
+	}
+
+	@Override
+	public boolean isFeatured() {
+		return this.featured;
+	}
+
+	@Override
+	public void toggleFeatured() {
+		this.featured = !this.isFeatured();
+	}
+	
+	@Override
+    public String toString() {
+        return "WallPost {" +
+            "text: " + getText() +
+            ", likes: '" + getLikes() + "'" +
+            ", featured: '" + isFeatured() + "'" +
+            "}";
+    }
+
+}
