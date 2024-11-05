71b10e1808217d30c93621c6b5a29c05a3d17450
Federico Kesselman <kesselmanfederico@gmail.com>
2024-11-05 13:58:06 -0300

    OO1 - EJ20 (clase)

---
+++ SEGUNDO AÑO/2do CUATRIMESTRE/Orientación a Objetos 1 (OO1)/EJ20-OO1/src/main/java/io/github/unlp_oo/EJ20_OO1/Empleado.java
@@ -0,0 +1,15 @@
+package io.github.unlp_oo.EJ20_OO1;
+import java.time.LocalDate;
+import java.util.*;
+
+
+public class Empleado {
+	private String nombre;
+	private String apellido;
+	private int CUIL;
+	private LocalDate fechaNacimiento;
+	private boolean hijos;
+	private List<Contrato> contratos;
+	
+	
+}
