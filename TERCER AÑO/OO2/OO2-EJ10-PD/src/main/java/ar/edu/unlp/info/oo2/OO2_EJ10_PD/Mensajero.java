6429bc9886c8bf1fe82b03077ee51479224c1cba
FedericoK <kesselmanfederico@gmail.com>
2025-04-29 19:35:46 -0300

    .

---
+++ TERCER AÑO/OO2/OO2-EJ10-PD/src/main/java/ar/edu/unlp/info/oo2/OO2_EJ10_PD/Mensajero.java
@@ -0,0 +1,13 @@
+package ar.edu.unlp.info.oo2.OO2_EJ10_PD;
+
+public class Mensajero {
+	private AlgoritmoCifrado algoritmo;
+	
+	public void enviar(String mensaje) {
+		
+	}
+	
+	public void recibir(String mensaje) {
+		
+	}
+}
