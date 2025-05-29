ae4bac0d4f3d534c90c1ea06fc8649bc390b2ccc
FedericoK <kesselmanfederico@gmail.com>
2025-05-29 15:34:18 -0300

    OO2 EJ4i Framework

---
+++ TERCER AÑO/OO2/Frameworks/tcp.server.repy/PasswordApp.java
@@ -0,0 +1,10 @@
+import tcp.server.reply.EchoHandler;
+import tcp.server.reply.PasswordHandler;
+import tcp.server.reply.SingleConnectionHandler;
+import tcp.server.reply.TCPControlLoop;
+
+public class PasswordApp {
+	public static void main(String[] args) {
+        new TCPControlLoop(new SingleConnectionHandler(new PasswordHandler())).startLoop(args);
+    }
+}
---
+++ TERCER AÑO/OO2/Frameworks/tcp.server.repy/tcp/server/reply/PasswordHandler.java
@@ -0,0 +1,60 @@
+package tcp.server.reply;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class PasswordHandler implements IMessageHandler {
+
+	@Override
+	public void handleMessage(String message, PrintWriter out) {
+		String[] args = message.split(" ");
+		
+		if (args.length != 3) {
+	        out.println("Error: Se deben enviar 3 parámetros.");
+	        return;
+	    }
+		
+		String caracteres = args[0];
+		String numeros = args[1];
+		String especiales = args[2];
+		
+		if (caracteres.isEmpty() || numeros.isEmpty() || especiales.isEmpty()) {
+		    out.println("Error: Ningún parámetro puede estar vacío.");
+		    return;
+		}
+		
+		out.println("Constraseña: " + passwordLogic(caracteres, numeros, especiales));
+	}
+	
+	private String passwordLogic(String letras, String numeros, String especiales) {
+	    Random random = new Random();
+	    StringBuilder password = new StringBuilder();
+
+	    // 1 número random
+	    char numero = numeros.charAt(random.nextInt(numeros.length()));
+	    
+	    // 1 carácter especial random
+	    char especial = especiales.charAt(random.nextInt(especiales.length()));
+	    
+	    // Seleccionar 6 letras random 
+	    List<Character> chars = new ArrayList<>();
+	    for (int i = 0; i < 6; i++) {
+	        chars.add(letras.charAt(random.nextInt(letras.length())));
+	    }
+
+	    chars.add(numero);
+	    chars.add(especial);
+
+	    // mezcla de caracteres
+	    Collections.shuffle(chars);
+	    
+	    for (char c : chars) {
+	        password.append(c);
+	    }
+
+	    return password.toString();
+	}
+}
