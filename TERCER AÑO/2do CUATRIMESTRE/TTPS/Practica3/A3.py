fb9192d8cfd5e6eb79b48885e2e6498fbbc24d87
FedericoK <kesselmanfederico@gmail.com>
2025-10-17 17:57:41 -0300

    taller

--- TERCER AÑO/2do CUATRIMESTRE/TTPS/Practica3/A3.py
+++ TERCER AÑO/2do CUATRIMESTRE/TTPS/Practica3/A3.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 n, m = map(int, input().split()) # lectura de n y m
 g = [[] for _ in range(n + 1)] # grafo, lista de listas
@@ -7,3 +8,25 @@ for _ in range(m):
     a, b = map(int, input().split()) # lectura de aristas
     g[a].append(b) # arsta a -> b
     g[b].append(a) # arista b -> a
+
+visitados = [False] * (n + 1) # nodos visitados
+representantes = [] # representante de cada componente. es el primer visitado
+
+def bfs(inicio):
+    q = deque([inicio]) # inicializa cola con nodo inicio
+    visitados[inicio] = True
+    while q: # mientras la cola no este vacia
+        u = q.popleft() # desencola
+        for v in g[u]: # vecinos de u 
+            if not visitados[v]: 
+                visitados[v] = True
+                q.append(v)
+
+for i in range(1, n+1):
+    if not visitados[i]:
+        representantes.append(i) # primero del componente
+        bfs(i) # recorrido a partir de ese nodo
+
+print(len(representantes)-1) # cantidad de rutas requeridas
+for i in range(1, len(representantes)):
+    print(representantes[i], representantes[i-1])
